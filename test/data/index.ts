658ac1d831a2b5ec3579ce4b2a57f3386011276e
Jason P <pritchardjasonr@gmail.com>
2023-09-21 12:00:28 -0600

    comment edit

--- test/data/index.ts
+++ test/data/index.ts
@@ -1,4 +1,4 @@
 /**
- * If any JOSN's of fixtures are added to use them in tests we will want to export them from the index
+ * If any Data Objects of fixtures are added to use them in tests we will want to export them from the index
  */
 export * from './test1';
